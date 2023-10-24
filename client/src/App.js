c5f3a45eda7a8bb69b5f001995ad6e512ffa4dd7
Husaam NT <husnaeem@gmail.com>
2023-10-24 14:49:41 +0100

    running react page

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div class="flex flex-row">
+            <div class="basis-1/4">Roundhouse</div>
+            <div class="basis-1/2">02</div>
+            <div class="basis-1/4">03</div>
+        </div>
+    );
+}
+
+export default App;
